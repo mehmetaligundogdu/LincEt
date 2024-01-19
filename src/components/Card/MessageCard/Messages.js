ef3f91b5a46a695b80dae048f8a14f6f04753a11
kodluyoruz <gundogdu.102@outlook.com>
2024-01-19 23:53:29 +0300

    Firebase RTDB imp

--- src/components/Card/MessageCard/Messages.js
+++ src/components/Card/MessageCard/Messages.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {FloatingButton} from '../../FloatingButton';
 import {ContentInputModal} from '../../Modal/ContentInputModal';
+import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
 
 import {styles} from './Messages.style';
 
@@ -12,8 +14,20 @@ const Messages = () => {
     setInputModalVisible(!inputModalVisible);
   }
   function handleSendContent(content) {
-    console.log(content);
+    handleInputToggle();
+    sendContent(content);
   }
+  function sendContent(content) {
+    const userMail = auth().currentUser.email;
+
+    const contentObj = {
+      text: content,
+      userName: userMail.split('@')[0],
+      date: new Date().toISOString(),
+    };
+    console.log(contentObj);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <FloatingButton icon="plus" onPress={handleInputToggle} />
